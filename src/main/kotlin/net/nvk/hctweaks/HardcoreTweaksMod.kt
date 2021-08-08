4b188191f27f1ad68a14770b6a910faae5931cdb
honzikv <honzikv@students.zcu.cz>
2021-08-08 10:11:25 +0200

    kotlin entrypoint init

---
+++ src/main/kotlin/net/nvk/hctweaks/HardcoreTweaksMod.kt
@@ -0,0 +1,10 @@
+package com.nvk.hctweaks
+
+import net.fabricmc.api.ModInitializer
+
+class HardcoreTweaksMod : ModInitializer {
+
+    override fun onInitialize() {
+        print("Hello Fabric.")
+    }
+}
